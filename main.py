b0bbbb654d0d6680dece5be9485a8885f5d16568
meron bossin <meronb1312@gmail.com>
2023-04-30 17:25:16 +0300

    adding more logic

--- main.py
+++ main.py
@@ -14,24 +14,15 @@ class MyApp(QObject):
 
     @pyqtSlot()
     def on_button_clicked(self):
-        self.update_zone("clicked")
-        lst = [{"taunter": "lr", "word": "truth"},{"taunter": "loo", "word": "listen"}]
-        self.block_change.emit(lst)
+        block_chain.NextBlock()
+        self.update_zone(block_chain.current_block.zone_holder)
+        self.block_change.emit(block_chain.current_block.action_list)
 
 qmlRegisterType(MyApp, "MyApp", 1, 0, "MyApp")
 app = QGuiApplication(sys.argv)
 engine = QQmlApplicationEngine()
 engine.quit.connect(app.quit)
 engine.load('main.qml')
-
-class Action:
-    taunter: str
-    word: str
-    me: bool
-
-    def __init__(self, taunter, word) -> None:
-        self.taunter = taunter
-        self.word = word
 
 class Block:
     action_list: list
@@ -54,17 +45,17 @@ class Block:
 
 class BlockChain:
     block_list: list
-    __index: int
+    __index: int = 0
+    current_block: Block
 
     def __init__(self, block_lst) -> None:
         self.block_list = block_lst
 
-    def NextBlock(self) -> Block:
-        block = self.block_list[self.__index]
+    def NextBlock(self):
+        self.current_block = self.block_list[self.__index]
         self.__index += 1
-        if(self.__index == self.block_list.count()):
+        if(self.__index == self.block_list.__len__):
             self.__index = 1
-        return block
 
     def __PrepareBlocks(self):
         for block in self.block_list:
@@ -72,31 +63,32 @@ class BlockChain:
         self.__index = 0
 
 start = Block(zone_holder="SC", 
-              action_list = [Action(taunter="LR", word="Truth"),
-                              Action(taunter="LOO", word="Listen")])
+              action_list = [{"taunter":"LR", "word":"Truth"},
+                              {"taunter":"LOO", "word":"Listen"}])
 block1 = Block(zone_holder="LR", 
-              action_list = [Action(taunter="AP", word="Truth"),
-                              Action(taunter="SC", word="Listen"),
-                              Action(taunter="SC", word="Truth")])
+              action_list = [{"taunter":"AP", "word":"Truth"},
+                              {"taunter":"SC", "word":"Listen"},
+                              {"taunter":"SC", "word":"Truth"}])
 block2 = Block(zone_holder="AP", 
-              action_list = [Action(taunter="LR", word="Listen"),
-                              Action(taunter="LR", word="Truth"),
-                              Action(taunter="LOO", word="Truth"),
-                              Action(taunter="LOO", word="Listen")])
+              action_list = [{"taunter":"LR", "word":"Listen"},
+                              {"taunter":"LR", "word":"Truth"},
+                              {"taunter":"LOO", "word":"Truth"},
+                              {"taunter":"LOO", "word":"Listen"}])
 block3 = Block(zone_holder="LOO", 
-              action_list = [Action(taunter="AP", word="Truth"),
-                              Action(taunter="SC", word="Listen"),
-                              Action(taunter="SC", word="Truth")])
+              action_list = [{"taunter":"AP", "word":"Truth"},
+                              {"taunter":"SC", "word":"Listen"},
+                              {"taunter":"SC", "word":"Truth"}])
 block4 = Block(zone_holder="SC", 
-              action_list = [Action(taunter="LR", word="Listen"),
-                              Action(taunter="LR", word="Truth"),
-                              Action(taunter="LOO", word="Truth"),
-                              Action(taunter="LOO", word="Listen")])
+              action_list = [{"taunter":"LR", "word":"Listen"},
+                              {"taunter":"LR", "word":"Truth"},
+                              {"taunter":"LOO", "word":"Truth"},
+                              {"taunter":"LOO", "word":"Listen"}])
 
 block_lst = [start, block1, block2, block3, block4]
 block_chain = BlockChain(block_lst = block_lst)
 
-mouse.on_button_event(block_chain.NextBlock, buttons=mouse.X2, types=mouse.DOWN)
+myApp = engine.rootObjects()[0].findChild(QObject, "myApp")
 
+mouse.on_button(block_chain.NextBlock, buttons=mouse.X2, types=mouse.DOWN)
 
 sys.exit(app.exec())
